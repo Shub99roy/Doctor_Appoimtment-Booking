ba75a66a20b5dd02b01b1724ac32c5d720474b10
shub99roy <shub99roy@oytlook.com>
2025-12-10 18:14:11 +0530

    Initial commit of my project

---
+++ XCare group/XCare/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>XCare simplifies healthcare by connecting patients, doctors, and vendors in one digital platform. We focus on making medical services more accessible and efficient through smart technology.
+
+</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li><a href="/" className="hover:underline cursor-pointer">Home</a></li>
+            <li><a href="/about" className="hover:underline cursor-pointer">About us</a></li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 6202951288</li>
+            <li>shub99roy@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ XCare.in - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ XCare group/XCare/src/pages/About.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-center text-2xl pt-10 text-[#707070]'>
+        <p>ABOUT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to XCare, At XCare, we aim to simplify and digitize healthcare services. Our platform connects patients, doctors, and medical vendors, making it easy to book appointments, access prescriptions, and manage care — all in one place.
+
+We’re focused on innovation, efficiency, and building a healthier future through technology.</p>
+          <p>XCare is dedicated to transforming the healthcare experience through innovation and simplicity.
+          We bring together patients, doctors, and vendors in one seamless platform, streamlining appointments, prescriptions, and communication. From your first consultation to ongoing care, XCare is with you—every step of the way</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>To revolutionize the healthcare ecosystem by creating a fully connected, intelligent, and paperless platform that empowers patients, doctors, and medical partners through seamless digital experiences and AI-driven innovation.
+
+</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY  <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>EFFICIENCY:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>CONVENIENCE: </b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>PERSONALIZATION:</b>
+          <p >Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default About
